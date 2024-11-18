f77e8268ec0f6bfb10da9e64d2b16398c1bcf9f6
HodeiC <hodeicasado@gmail.com>
2024-11-18 18:52:36 +0100

    Modificado el programa principal

--- programaPrincipal.c
+++ programaPrincipal.c
@@ -36,7 +36,7 @@ int main(void)
 
             case 2:
                 leerCalidadDelSueño(&calidadDelSueño);
-                printf("Se ha cargado la calidad del sueño\n\n;);
+                printf("Se ha cargado la calidad del sueño\n\n");
 
 
         }
@@ -53,15 +53,70 @@ int main(void)
 
 void leeNivelEstres(NivelEstres * CS)
 {
-    printf("introduzca su edad: ");
+    int horas, minutos;
+    float tiempo;
+
+    printf("Introduce su edad: ");
     scanf("%d",&CS -> edad);
     printf("\n");
 
-    printf("Introduca su genero: ");
+    printf("Introduce su genero: ");
     scanf("%s",&CS -> genero);
+    printf("\n");
 
+    printf("Introduce el año de universidad en el que se encuentra: ");
+    scanf("%d", &CS -> año_universidad);
+    printf("\n");
 
-    printf("Introduca );
+    printf("Introduce el numero de horas y minutos que has DORMIDO en este formato (horas minutos): ");
+    scanf("%d" "%d", &horas, &minutos);
+    minutos = ((minutos * 100) / 60) / 100;  // para pasar de minutos a la coma -> 30 mins = 0.50 horas
+    CS -> horas_sueño = horas + minutos;
+    printf("\n");
+
+    printf("Introduce el numero de horas y minutos que has ESTUDIADO en este formato (horas minutos): ");
+    scanf("%d" "%d", &horas, &minutos);
+    minutos = ((minutos * 100) / 60) / 100;
+    CS -> horas_estudio = horas + minutos;
+    printf("\n");
+
+    printf("Introduce el numero de horas y minutos que has ESTADO MIRANDO PANTALLAS en este formato (horas minutos): ");
+    scanf("%d" "%d", &horas, &minutos);
+    minutos = ((minutos * 100) / 60) / 100;
+    CS -> horas_pantalla = horas + minutos;
+    printf("\n");
+
+    printf("Introduce los gramos de cafeina que ha tomado: ");
+    scanf("%f", &CS -> cafeina);
+    printf("\n");
+
+    printf("Introduce cuanta actividad fisica ha hecho (0 = Nada / 100 = Mucha): ")
+    scanf("%d", &CS -> actividad_fisica);
+    printf("\n");
+
+    printf("Introduce a que hora te sueles acostar en la semana (hora minutos): ");
+    scanf("%d" "%d", &horas, &minutos);
+    minutos = ((minutos * 100) / 60) / 100;
+    CS -> acostar_semana = horas + minutos;
+    printf("\n");
+
+    printf("Introduce a que hora te sueles acostar en el fin de semana (hora minutos): ");
+    scanf("%d" "%d", &horas, &minutos);
+    minutos = ((minutos * 100) / 60) / 100;
+    CS -> acostar_finde = horas + minutos;
+    printf("\n");
+
+    printf("Introduce a que hora te sueles despertar en la semana (hora minutos): ");
+    scanf("%d" "%d", &horas, &minutos);
+    minutos = ((minutos * 100) / 60) / 100;
+    CS -> despertar_semana = horas + minutos;
+    printf("\n");
+
+    printf("Introduce a que hora te sueles despertar en el fin de semana (hora minutos): ");
+    scanf("%d" "%d", &horas, &minutos);
+    minutos = ((minutos * 100) / 60) / 100;
+    CS -> despertar_finde = horas + minutos;
+    printf("\n");
 
 
 
