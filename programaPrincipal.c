2a25eb92cc5ab28f80bcb1fa3b245aa192713998
HodeiC <hodeicasado@gmail.com>
2024-11-25 18:43:26 +0100

    Programa principal modificado

--- programaPrincipal.c
+++ programaPrincipal.c
@@ -6,6 +6,11 @@
 
 
 void leerCalidadDelSueño(calidadDelSueño *);
+void determinarCalidadSueño(listaOrdenadaVecinos , calidadDelSueño *);
+void imprimirResultados(calidadDelSueño, listaOrdenadaVecinos);
+void compruebaDatos(listaEstudiantes *,int );
+void depuraListaEstudiantes(listaEstudiantes*,int);
+
 
 int main(void)
 {
@@ -13,7 +18,7 @@ int main(void)
     listaEstudiantes lista;
     listaOrdenadaVecinos listaVecinos;
 
-    int opcion;
+    int opcion, numVecinos_K;
 
 
     do
@@ -21,6 +26,16 @@ int main(void)
         printf("------------MENU------------\n");
         printf("\t1 - Cargar dataset\n");
         printf("\t2 - Introducir la calidad del sueño\n");
+        printf("\t3 - Normalizar datos (Debe haberse realizado los pasos 1 y 2)\n");
+        printf("\t4 - Introducir/cambiar el numero de vecinos\n");
+        printf("\t5 - Comprobar calidad del Sueño\n");
+        printf("\t6 - Introducir calidad del sueño al dataset\n");
+        printf("\t7 - Comprobar efectividad del dataset\n");
+        printf("\t8 - Depurar lista\n");
+        printf("\t9 - Imprimir dataset\n");
+        printf("\t10 - Salir.\n");
+
+
 
 
         printf("Escoja una opcion: ");
@@ -33,13 +48,80 @@ int main(void)
 				lista = cargarDatos();
                 printf("El dataset ha sido cargado correctamente\n\n");
 
-
             case 2:
                 leerCalidadDelSueño(&calidadDelSueño);
                 printf("Se ha cargado la calidad del sueño\n\n");
 
+            case 3:
+                normalizar(&lista, &calidadDelSueño);
+                printf("Datos normalizados\n\n"):
+
+            case 4:
+                printf("Introduce un valor para k (k > 0) que representa el numero de vecinos: \n");
+                scanf("%d", &numVecinos_K)
+
+                while(numVecinos_K <= 0)
+                {
+                    printf("ERROR - Introduce un valor mayor que 0: ");
+                    scanf("%d",&numVecinos_K);
+                }
+
+                printf("\n\n");
+
+            case 5:
+                iniciarlistavecinos(&listaVecinos);
+                listaVecinos = distanciaminima(calidadDelSueño, lista, numVecinos_K);
+                determinarCalidadSueño(&listaVecinos, &CalidadDelSueño);
+                imprimirResultados(CalidadDelSueño, listaVecinos);
+
+                printf("\n\n");
+
+            case 6:
+                añadirDato(&lista, &calidadDelSueño);
+
+                printf("\n\n");
+
+            case 7:
+                compruebaDatos(&lista,numVecinos_K);
+                printf("\n\n");
+
+            case 8:
+                depuraListaEstudiantes(&lista,numVecinos_K);
+                printf("\n\n");
+
+            case 9:
+                imprimeListaEntera(lista);
+                printf("\n\n");
+
+            case 10:
+                printf("\n\tSaliendo del programa\n");
+                Nodo *borrar;
+                borrar = lista.primero;
+                while (borrar != NULL)
+                {
+                    desencolar (&lista);
+                    borrar = borrar->siguiente;
+                }
+
+                celdavecino *vecinito;
+
+                while (!estavacia(listaVecinos))
+                {
+                    vecinito = listaVecinos.primero;
+                    listaVecinos.primero = listaVecinos.primero->siguiente;
+                    free(vecinito);
+                }
+
+				break;
+
+
+
+            default:
+                printf("\nERROR: La opcion seleccionada no esta entre las opciones, vuelva a intentarlo.\n");
+
 
         }
+
 
 
     }while(opcion != );
@@ -60,8 +142,8 @@ void leeNivelEstres(NivelEstres * CS)
     scanf("%d",&CS -> edad);
     printf("\n");
 
-    printf("Introduce su genero: ");
-    scanf("%s",&CS -> genero);
+    printf("Introduce su genero (Mujer = 0 || Hombre = 1): ");
+    scanf("%d",&CS -> genero);
     printf("\n");
 
     printf("Introduce el año de universidad en el que se encuentra: ");
@@ -117,6 +199,53 @@ void leeNivelEstres(NivelEstres * CS)
     minutos = ((minutos * 100) / 60) / 100;
     CS -> despertar_finde = horas + minutos;
     printf("\n");
+
+}
+
+
+
+
+
+void determinarCalidadSueño(listaOrdenadaVecinos l, calidadDelSueño *CS)
+{
+    CS -> calidad_sueño = mediaVecinos(l);
+    printf("La calidad de sueño del estudiante es: %d\n", CS -> calidad_sueño);
+
+    printf("\n");
+}
+
+void imprimirResultados(calidadDelSueño CS, listaOrdenadaVecinos l)
+{
+    printf("El nivel de calidad de sueño a comprobar ha resultado ser: %d\n", mediaVecinos(l));
+    celdavecino * auxiliar;
+    auxiliar = l.primero;
+    int i = 1;
+    while (auxiliar != NULL){
+        printf("La distancia a la calidad de sueño %d mas cercano ha sido %f y en el dataset pertenece a la posicion %d: \n", i, auxiliar->distancia,  auxiliar ->id);
+        imprimeDato(auxiliar->celdaEstudiantes->dato);
+        auxiliar = auxiliar -> siguiente;
+        i++;
+        printf ("----------------------------------------------------------------------------------\n");
+    }
+
+}
+
+
+void compruebaDatos(listaEstudiantes *l,int K)
+{
+
+
+
+
+}
+
+
+void depuraListaEstudiantes(listaEstudiantes*,int)
+{
+    listaEstudiantes listaAuxiliar;
+    listaOrdenadaVecinos vecinos;
+
+
 
 
 
